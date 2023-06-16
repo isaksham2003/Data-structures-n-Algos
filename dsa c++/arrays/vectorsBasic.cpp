bb864a4ba4bef1acff545635e199efd7194378a7
Saksham <saksham3541stella@gmail.com>
2023-06-16 20:36:43 +0530

    first

---
+++ dsa c++/arrays/vectorsBasic.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(vector<int>&v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<endl;
+    }
+}
+int main()
+{
+vector<int>a;
+int n,element;  //need to take another variable for elements
+cout<<"enter size of vector"<<endl;
+cin>>n;
+cout<<endl<<"enter elements in vector: ";
+for(int i=0;i<n;i++)
+{
+cin>>element;
+a.push_back(element);
+}
+display(a);
+return 0;
+}
+
